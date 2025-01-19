700f18f7967b0e94777ebf87c8edcfb840608e9f
AusGadgets <chris.davidson@cantrayit.com.au>
2025-01-20 00:01:15 +0800

    Update jbd_bms_ble.cpp

--- components/jbd_bms_ble/jbd_bms_ble.cpp
+++ components/jbd_bms_ble/jbd_bms_ble.cpp
@@ -338,17 +338,17 @@ void JbdBmsBle::on_hardware_version_data_(const std::vector<uint8_t> &data) {
   //           0x41 0x2D 0x42 0x2D 0x55
 //  this->device_model_ = std::string(data.begin(), data.end());
   
-  this->manufacturer_name_ = std::string(data.begin(), data.end());
-  this->hardware_revision_ = std::string(data.begin(), data.end());
+//  this->manufacturer_name_ = std::string(data.begin(), data.end());
+ // this->hardware_revision_ = std::string(data.begin(), data.end());
  
   this->serial_number_ = std::string(data.begin(), data.end());
 
   ESP_LOGI(TAG, "  Model name: %s", this->device_model_.c_str());
   
   // this->publish_state_(this->device_model_text_sensor_, this->device_model_text_sensor_);
-  this->publish_state_(this->manufacturer_name_text_sensor_, this->manufacturer_name_text_sensor_);
+//  this->publish_state_(this->manufacturer_name_text_sensor_, this->manufacturer_name_text_sensor_);
  
-  this->publish_state_(this->serial_number_text_sensor_, this->serial_number_text_sensor_);
+//  this->publish_state_(this->serial_number_text_sensor_, this->serial_number_text_sensor_);
 }
 
 void JbdBmsBle::track_online_status_() {
